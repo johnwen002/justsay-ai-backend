69e7df8f2606aa9fd20b33472854e7580b0bed76
jackson.wen <jackson.wen@xm.wonder.com>
2025-05-15 20:16:31 +0800

    feat: Add category_name field to FollowRSSPath model

--- src/models/follow.py
+++ src/models/follow.py
@@ -7,3 +7,4 @@ class FollowRSSPath(BaseSQLModel, table=True):
     __tablename__ = "follow_rss_path"
     user_id: str = Field(nullable=True)
     rss_path: str = Field(nullable=True)
+    category_name: str = Field(nullable=False, index=True)
